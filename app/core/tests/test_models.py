dc64515c579e763bebd1248c29533c83c01c9ca4
Sakhawat0pu <s19hossain98@gmail.com>
2024-02-26 21:47:23 -0500

    Configured model.py by creating customr user model to create user

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,36 @@
+"""
+Test for models.
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelsTest(TestCase):
+    """Test Models."""
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with email is successful"""
+        email = "test@example.com"
+        password = "testPass123"
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new users"""
+
+        sample_emails = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.com", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"],
+            ["test5@example.Com", "test5@example.com"],
+        ]
+
+        for email, expected_email in sample_emails:
+            user = get_user_model().objects.create_user(
+                email=email, password="sample123"
+            )
+            self.assertEqual(user.email, expected_email)
